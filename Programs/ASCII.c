0d8b46b43b964878079d41d980ef06af6ddee5c9
Mayur Dilip Tawade <87850085+Mayuralf2001@users.noreply.github.com>
2022-08-02 00:07:58 +0530

    commit ASCII

---
+++ Programs/ASCII.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+    printf("ASCII Value");
+
+    char c;
+    printf("\nEnter a character :");
+    scanf("%c",&c);
+
+    printf("ASCII value is %c = %d ", c,c);
+
+    return 0;
+}
